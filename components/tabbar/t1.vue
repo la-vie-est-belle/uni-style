5b126b07c76fa2e91ecd7f2258dbd8d86a014e7b
la vie <louasure@126.com>
2021-11-11 11:51:12 +0800

    added one tabbar style

--- components/tabbar/t1.vue
+++ components/tabbar/t1.vue
@@ -1,22 +1,69 @@
 <template>
-	<view>
-		
+	<view class="tabbar">
+		<view @click="changeTab(item.text)" v-for="(item, index) in tabs" :key="index" class="one-tab">
+			<view><image class="tab-icon" :src="currentText==item.text ? item.selectedIconPath : item.iconPath"></image></view>
+			<view><text class="tab-text" :style="currentText==item.text ?'color:black;' : 'color:#999999' ">{{ item.text }}</text></view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
-		data() {
-			return {
-				
+		props: {
+			tabs: {
+				type: Array,
+				default: () => {[]}
 			}
 		},
+		
+		data() {
+			return {
+				currentText: "首页"
+			}
+		},
+		
 		methods: {
-			
+			changeTab(text) {
+				if (this.currentText == text) {
+					return;
+				}
+				
+				this.currentText = text;
+				this.$emit('tabChanged', text)
+			}
 		}
 	}
 </script>
 
 <style>
+.tabbar {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 100%;
+	height: 135rpx;
+	background-color:white;
+	/* border-top: #C0C0C0 1px solid; */
+/* 	border-top-left-radius: 10rpx;
+	border-top-right-radius: 10rpx; */
+	z-index: 1000;
+}
+
+.one-tab {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+}
+
+.tab-icon {
+	width: 50rpx;
+	height: 50rpx;
+	margin-bottom: -13rpx;
+}
+
+.tab-text {
+	font-size: 25rpx;
+}
 
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,24 +1,55 @@
 <template>
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
+		<view v-for="(item,index) in 1000"class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+		<view class="tabbar"><tabbar :tabs="tabs" @tabChanged=""></tabbar></view>
 	</view>
 </template>
 
 <script>
+	import tabbar from "../../components/tabbar/t1.vue"
 	export default {
+		components: {
+			tabbar
+		},
 		data() {
 			return {
-				title: 'Hello'
+				title: "Hello",
+				tabs: [
+					{
+						"text": "首页",
+						"iconPath": "../../static/tabbar/home.png",
+						"selectedIconPath": "../../static/tabbar/homeSelected.png",
+					},
+					{
+						"text": "购物",
+						"iconPath": "../../static/tabbar/shop.png",
+						"selectedIconPath": "../../static/tabbar/shopSelected.png",
+					},
+					{
+						"text": "消息",
+						"iconPath": "../../static/tabbar/message.png",
+						"selectedIconPath": "../../static/tabbar/messageSelected.png",
+					},
+					{
+						"text": "我的",
+						"iconPath": "../../static/tabbar/person.png",
+						"selectedIconPath": "../../static/tabbar/personSelected.png",
+					}
+				]
 			}
 		},
+		
 		onLoad() {
 
 		},
+		
 		methods: {
-
+			changeTab(text) {
+				console.log(text)
+			}
 		}
 	}
 </script>
@@ -48,5 +79,11 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	
+	.tabbar {
+		position: fixed;
+		left: 0rpx;
+		bottom: 0rpx;
 	}
 </style>
